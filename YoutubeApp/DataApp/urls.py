1cf1d1f59f57e1d67ea3d58078b2fcd88eeb68b9
Darren B <68653294+Devd0@users.noreply.github.com>
2021-08-06 10:08:23 -0400

    New Ui

--- YoutubeApp/DataApp/urls.py
+++ YoutubeApp/DataApp/urls.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
     path('product/', views.product),
-    path('apiData/', views.apiData)
+    path('apiData/', views.apiData),
 ]
+
+
--- YoutubeApp/DataApp/views.py
+++ YoutubeApp/DataApp/views.py
@@ -4,18 +4,23 @@ from googleapiclient.discovery import build
 from .models import *
 
 
+
 # Create your views here.
 # API key: AIzaSyC7R7jYq97okNNfWGfbhWWmK8fXZ1D-eig
+# Testing id: UCqZQlzSHbVJrwrn5XvzrzcA
 
 def home(request):
     return render(request, 'DataApp/home.html')
 
+
 def product(request):
 
     Image_url = "https://i.pinimg.com/originals/7d/c9/93/7dc993c70d4adba215b87cafdc59d82d.png"
+    Description = "------"
 
     context = {
         "Image": Image_url,
+        "Description": Description,
     }
 
     return render(request, 'DataApp/product.html', context)
@@ -26,8 +31,8 @@ def apiData(request):
 
     api_key = 'AIzaSyC7R7jYq97okNNfWGfbhWWmK8fXZ1D-eig'
     youtube = build('youtube','v3', developerKey=api_key)
-
-    # Api Request
+    
+    # Api Requests
     request1 = youtube.channels().list(
         part='statistics',
         id= "{}".format(request.POST['stats'])
@@ -37,25 +42,57 @@ def apiData(request):
         part='snippet',
         id= "{}".format(request.POST['stats'])
     )
+
+
+    request3 = youtube.playlists().list(
+        part='snippet',
+        channelId= "{}".format(request.POST['stats'])
+    )
+
+    request4 = youtube.channels().list(
+        part='contentOwnerDetails',
+        id="{}".format(request.POST['stats'])
+        
+        
+    )
+
     
 
     try:
         response = request1.execute()
         response2 = request2.execute()
+        
         Data = f"Total Subscribers: {response['items'][0].get('statistics').get('subscriberCount')}"
         Data2 = f"Total Views: {response['items'][0].get('statistics').get('viewCount')}"
         Data3 = f"Total Videos:  {response['items'][0].get('statistics').get('videoCount')}"
+        Description = f"{response2['items'][0].get('snippet').get('description')}"
+        Location =f"{response2['items'][0].get('snippet').get('country')}"
         Image_url = f"{response2['items'][0].get('snippet').get('thumbnails').get('default').get('url')}"
+        Channel = f"{response2['items'][0].get('snippet').get('title')}"
+        Url = "https://www.youtube.com/channel/{}".format(request.POST['stats'])
+
+        print(Description)
+        
     except Exception:
-        Data = "The Channel Username/Id above is not valid"
-        Image_url = ""
+        Url =""
+        Data = "The Channel ID that you submitted is not valid."
+        Data2 = ""
+        Data3 = ""
+        Description = "----"
+        Location = "----"
+        Image_url = "https://i.pinimg.com/originals/7d/c9/93/7dc993c70d4adba215b87cafdc59d82d.png"
+        Channel = ""
+        
+    
     
     # Saves Channel data
     new_data = User_data(stats = request.POST.get('stats'))
     new_data.save()
 
-    # Channel/username associated with data 
-    Channel = f"{response2['items'][0].get('snippet').get('title')}"
+    if Description == "":
+        Description = "Not Applicable"
+
+    
 
     context = {
         "Data": Data,
@@ -63,6 +100,9 @@ def apiData(request):
         "Data3": Data3,
         "Channel": Channel,
         "Image": Image_url,
+        "Url": Url,
+        "Description": Description,
+        "Location": Location,
     }
 
     return render(request, 'DataApp/product.html', context)
